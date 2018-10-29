0a48431da28849fcfeb330222d98f6433189e9ff
maninderpal <moneysingh1991@ymail.com>
2018-10-28 23:49:09 -0700

    added

---
+++ WebServer/src/main/java/com/wishyHub/WebServer/FileUploader.java
@@ -0,0 +1,111 @@
+package com.wishyHub.WebServer;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.multipart.MultipartFile;
+
+
+
+@RestController
+public class FileUploader{
+    
+   
+     //Save the uploaded file to this folder
+    private static final String UPLOADED_FOLDER = "Upload/";
+  // 3.1.2 Multiple file upload
+    @PostMapping("/upload")
+    public ResponseEntity<?> uploadFileMulti(
+            @RequestParam("extraField") String extraField,
+            @RequestParam("files") MultipartFile[] uploadfiles) {
+
+       
+            int ret = 0  ;
+        // Get file name
+        String uploadedFileName = Arrays.stream(uploadfiles).map(x -> x.getOriginalFilename())
+                .filter(x -> !StringUtils.isEmpty(x)).collect(Collectors.joining(" , "));
+
+        if (StringUtils.isEmpty(uploadedFileName)) {
+            return new ResponseEntity("please select a file!", HttpStatus.OK);
+        }
+
+        try {
+
+             ret = saveUploadedFiles(Arrays.asList(uploadfiles));
+
+        } catch (Exception e) {
+            if(e instanceof MaxUploadSizeExceededException) {
+                 return new ResponseEntity<>("Exception: File Size exceed: " +Integer.toString(ret),HttpStatus.OK);
+            } else {
+                 return new ResponseEntity<>("Something wrong in FileUploader: " +Integer.toString(ret),HttpStatus.BAD_REQUEST);
+            }
+           
+        }
+
+        return new ResponseEntity("Successfully uploaded - "
+                + uploadedFileName, HttpStatus.OK);
+
+    }
+//save file
+    private int saveUploadedFiles(List<MultipartFile> files) throws IOException {
+       String extension = "extra";
+       String dir= "";
+       byte[] bytes = null;
+        for (MultipartFile file : files) {
+
+            if (file.isEmpty()) {
+                continue; //next pls
+            }
+            
+            File directory = new File( UPLOADED_FOLDER);
+                if (! directory.exists()){
+                  if(directory.mkdir()) {
+                      System.out.println("############# success create directory");
+                  } else {
+                  System.out.println("############# Failed create directory............");
+                  }
+        // If you require it to make the entire directory path including parents,
+        // use directory.mkdirs(); here instead.
+              }
+                
+         
+
+             bytes = file.getBytes();
+            
+            
+            extension = file.getOriginalFilename();
+            extension = extension.substring(extension.lastIndexOf(".")+1);
+                   dir = UPLOADED_FOLDER + extension+"/";
+
+            directory = new File( dir);
+                if (! directory.exists()){
+                  directory.mkdir();
+        // If you require it to make the entire directory path including parents,
+        // use directory.mkdirs(); here instead.
+              }
+            
+            Path path = Paths.get(dir+  file.getOriginalFilename());
+            
+            
+            Files.write(path, bytes);
+            
+
+        }
+        return bytes.length;  
+    }
+
+   
+}
---
+++ WebServer/src/main/java/com/wishyHub/WebServer/TomcatController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.wishyHub.WebServer;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author maninderpal
+ */
+@RestController
+public class TomcatController {
+ 
+    @GetMapping("/hello")
+    public Collection<String> sayHello() {
+        return IntStream.range(0, 10)
+          .mapToObj(i -> "Hello number " + i)
+          .collect(Collectors.toList());
+    }
+}
