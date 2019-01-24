e80c535d4e1a4fbc997af27728ea003700cd97ee
Maninderpal Singh <moneysingh1991@users.noreply.github.com>
2019-01-23 21:42:29 -0800

    Update PostController.java

--- WebServer/src/main/java/com/wishyHub/WebServer/Post/PostController.java
+++ WebServer/src/main/java/com/wishyHub/WebServer/Post/PostController.java
@@ -100,7 +100,7 @@ public class PostController {
        return "success save post";
     }
     
-    @DeleteMapping("/delete")
+    @PostMapping("/delete")
     public HashMap<String, String> deletePost(@RequestParam("id") int id ) {
        // model.addAttribute("name", name);
        HashMap<String,String> map = new HashMap<String,String>();
